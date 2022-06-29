f774d4c8412a38849d8ef88de6461b6069da9b32
Couto1411 <gabriel.couto14@hotmail.com>
2022-06-29 15:30:37 -0300

    resistência

---
+++ js/script.js
@@ -0,0 +1,220 @@
+function hide() {
+    document.getElementById("resistor-table1").style.display = "none";
+    document.getElementById("resistor-table2").style.display = "none";
+    document.getElementById("resistor-table3").style.display = "none";
+    document.getElementById("resistor-table4").style.display = "none";
+}
+function brand1() {
+    document.getElementById("resistor-table1").style.display = "inline";
+    document.getElementById("resistor-table2").style.display = "none";
+    document.getElementById("resistor-table3").style.display = "none";
+    document.getElementById("resistor-table4").style.display = "none";
+}
+function brand2() {
+    document.getElementById("resistor-table1").style.display = "none";
+    document.getElementById("resistor-table2").style.display = "inline";
+    document.getElementById("resistor-table3").style.display = "none";
+    document.getElementById("resistor-table4").style.display = "none";
+}
+function brand3() {
+    document.getElementById("resistor-table1").style.display = "none";
+    document.getElementById("resistor-table2").style.display = "none";
+    document.getElementById("resistor-table3").style.display = "inline";
+    document.getElementById("resistor-table4").style.display = "none";
+}
+function brand5() {
+    document.getElementById("resistor-table1").style.display = "none";
+    document.getElementById("resistor-table2").style.display = "none";
+    document.getElementById("resistor-table3").style.display = "none";
+    document.getElementById("resistor-table4").style.display = "inline";
+}
+function faixa1P(){document.getElementById("brand1").style.backgroundColor = "black";document.getElementById("brand1").style.borderColor = "black";resistTot();}
+function faixa1M(){document.getElementById("brand1").style.backgroundColor = "brown";document.getElementById("brand1").style.borderColor = "brown";resistTot();}
+function faixa1Vm(){document.getElementById("brand1").style.backgroundColor = "red";document.getElementById("brand1").style.borderColor = "red";resistTot();}
+function faixa1L(){document.getElementById("brand1").style.backgroundColor = "orange";document.getElementById("brand1").style.borderColor = "orange";resistTot();}
+function faixa1Am(){document.getElementById("brand1").style.backgroundColor = "yellow";document.getElementById("brand1").style.borderColor = "yellow";resistTot();}
+function faixa1Vd(){document.getElementById("brand1").style.backgroundColor = "green";document.getElementById("brand1").style.borderColor = "green";resistTot();}
+function faixa1Az(){document.getElementById("brand1").style.backgroundColor = "blue";document.getElementById("brand1").style.borderColor = "blue";resistTot();}
+function faixa1Vi(){document.getElementById("brand1").style.backgroundColor = "violet";document.getElementById("brand1").style.borderColor = "violet";resistTot();}
+function faixa1C(){document.getElementById("brand1").style.backgroundColor = "gray";document.getElementById("brand1").style.borderColor = "gray";resistTot();}
+function faixa1B(){document.getElementById("brand1").style.backgroundColor = "white";document.getElementById("brand1").style.borderColor = "white";resistTot();}
+
+function faixa2P(){document.getElementById("brand2").style.backgroundColor = "black";document.getElementById("brand2").style.borderColor = "black";resistTot();}
+function faixa2M(){document.getElementById("brand2").style.backgroundColor = "brown";document.getElementById("brand2").style.borderColor = "brown";resistTot();}
+function faixa2Vm(){document.getElementById("brand2").style.backgroundColor = "red";document.getElementById("brand2").style.borderColor = "red";resistTot();}
+function faixa2L(){document.getElementById("brand2").style.backgroundColor = "orange";document.getElementById("brand2").style.borderColor = "orange";resistTot();}
+function faixa2Am(){document.getElementById("brand2").style.backgroundColor = "yellow";document.getElementById("brand2").style.borderColor = "yellow";resistTot();}
+function faixa2Vd(){document.getElementById("brand2").style.backgroundColor = "green";document.getElementById("brand2").style.borderColor = "green";resistTot();}
+function faixa2Az(){document.getElementById("brand2").style.backgroundColor = "blue";document.getElementById("brand2").style.borderColor = "blue";resistTot();}
+function faixa2Vi(){document.getElementById("brand2").style.backgroundColor = "violet";document.getElementById("brand2").style.borderColor = "violet";resistTot();}
+function faixa2C(){document.getElementById("brand2").style.backgroundColor = "gray";document.getElementById("brand2").style.borderColor = "gray";resistTot();}
+function faixa2B(){document.getElementById("brand2").style.backgroundColor = "white";document.getElementById("brand2").style.borderColor = "white";resistTot();}
+
+function faixa3P(){document.getElementById("brand3").style.backgroundColor = "black";document.getElementById("brand3").style.borderColor = "black";resistTot();}
+function faixa3M(){document.getElementById("brand3").style.backgroundColor = "brown";document.getElementById("brand3").style.borderColor = "brown";resistTot();}
+function faixa3Vm(){document.getElementById("brand3").style.backgroundColor = "red";document.getElementById("brand3").style.borderColor = "red";resistTot();}
+function faixa3L(){document.getElementById("brand3").style.backgroundColor = "orange";document.getElementById("brand3").style.borderColor = "orange";resistTot();}
+function faixa3Am(){document.getElementById("brand3").style.backgroundColor = "yellow";document.getElementById("brand3").style.borderColor = "yellow";resistTot();}
+function faixa3Vd(){document.getElementById("brand3").style.backgroundColor = "green";document.getElementById("brand3").style.borderColor = "green";resistTot();}
+function faixa3Az(){document.getElementById("brand3").style.backgroundColor = "blue";document.getElementById("brand3").style.borderColor = "blue";resistTot();}
+function faixa3Vi(){document.getElementById("brand3").style.backgroundColor = "violet";document.getElementById("brand3").style.borderColor = "violet";resistTot();}
+function faixa3C(){document.getElementById("brand3").style.backgroundColor = "gray";document.getElementById("brand3").style.borderColor = "gray";resistTot();}
+function faixa3B(){document.getElementById("brand3").style.backgroundColor = "white";document.getElementById("brand3").style.borderColor = "white";resistTot();}
+function faixa3G(){document.getElementById("brand3").style.backgroundColor = "gold";document.getElementById("brand3").style.borderColor = "gold";resistTot();}
+function faixa3S(){document.getElementById("brand3").style.backgroundColor = "silver";document.getElementById("brand3").style.borderColor = "silver";resistTot();}
+
+function faixa5M(){document.getElementById("brand5").style.backgroundColor = "brown";document.getElementById("brand5").style.borderColor = "brown";resistTot();}
+function faixa5Vm(){document.getElementById("brand5").style.backgroundColor = "red";document.getElementById("brand5").style.borderColor = "red";resistTot();}
+function faixa5Vd(){document.getElementById("brand5").style.backgroundColor = "green";document.getElementById("brand5").style.borderColor = "green";resistTot();}
+function faixa5Az(){document.getElementById("brand5").style.backgroundColor = "blue";document.getElementById("brand5").style.borderColor = "blue";resistTot();}
+function faixa5Vi(){document.getElementById("brand5").style.backgroundColor = "violet";document.getElementById("brand5").style.borderColor = "violet";resistTot();}
+function faixa5C(){document.getElementById("brand5").style.backgroundColor = "gray";document.getElementById("brand5").style.borderColor = "gray";resistTot();}
+function faixa5G(){document.getElementById("brand5").style.backgroundColor = "gold";document.getElementById("brand5").style.borderColor = "gold";resistTot();}
+function faixa5S(){document.getElementById("brand5").style.backgroundColor = "silver";document.getElementById("brand5").style.borderColor = "silver";resistTot();}
+
+function resistTot(){
+    let resistTot;
+    let brand1 = document.getElementById("brand1").style.backgroundColor;
+    if (brand1) {
+        switch (brand1) {
+            case "black":
+                resistTot="0";
+                break;
+            case "brown":
+                resistTot="1";
+                break;
+            case "red":
+                resistTot="2";
+                break;
+            case "orange":
+                resistTot="3";
+                break;
+            case "yellow":
+                resistTot="4";
+                break;
+            case "green":
+                resistTot="5";
+                break;
+            case "blue":
+                resistTot="6";
+                break;
+            case "violet":
+                resistTot="7";
+                break;
+            case "gray":
+                resistTot="8";
+                break;
+            case "white":
+                resistTot="9";
+                break;
+            default:
+                break;
+        }
+        let brand2 = document.getElementById("brand2").style.backgroundColor;
+        switch (brand2) {
+            case "black":
+                resistTot+='0';
+                break;
+            case "brown":
+                resistTot+='1';
+                break;
+            case "red":
+                resistTot+='2';
+                break;
+            case "orange":
+                resistTot+='3';
+                break;
+            case "yellow":
+                resistTot+='4';
+                break;
+            case "green":
+                resistTot+='5';
+                break;
+            case "blue":
+                resistTot+='6';
+                break;
+            case "violet":
+                resistTot+='7';
+                break;
+            case "gray":
+                resistTot+='8';
+                break;
+            case "white":
+                resistTot+='9';
+                break;
+            default:
+                break;
+        }
+        let brand3 = document.getElementById("brand3").style.backgroundColor;
+        switch (brand3) {
+            case "brown":
+                resistTot+='0';
+                break;
+            case "red":
+                resistTot+="00";
+                break;
+            case "orange":
+                resistTot+="K";
+                break;
+            case "yellow":
+                resistTot+="0K";
+                break;
+            case "green":
+                resistTot+="00K";
+                break;
+            case "blue":
+                resistTot+="M";
+                break;
+            case "violet":
+                resistTot+="0M";
+                break;
+            case "gray":
+                resistTot+="00M";
+                break;
+            case "white":
+                resistTot+="G";
+                break;
+            case "gold":
+                resistTot=(parseInt(resistTot)/10)
+                break;
+            case "silver":
+                resistTot=(parseInt(resistTot)/100)
+                break;
+            default:
+                break;
+        }
+        resistTot+=" Ω";
+        let brand5 = document.getElementById("brand5").style.backgroundColor;
+        if (brand5) {
+            switch (brand5) {
+                case "brown":
+                    resistTot+=" ± 1%";
+                    break;
+                case "red":
+                    resistTot+=" ± 2%";
+                    break;
+                case "green":
+                    resistTot+=" ± 0.5%";
+                    break;
+                case "blue":
+                    resistTot+=" ± 0.25%";
+                    break;
+                case "violet":
+                    resistTot+=" ± 0.1%";
+                    break;
+                case "gray":
+                    resistTot+=" ± 0.05%";
+                    break;
+                case "gold":
+                    resistTot+=" ± 5%";
+                    break;
+                case "silver":
+                    resistTot+=" ± 0.01%";
+                    break;
+                default:
+                    break;
+            }
+        }
+        document.getElementById("resist-tot").innerText=resistTot;
+    }
+}
