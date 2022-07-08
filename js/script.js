69e47a9899b30aa5c43225708893df1ef8db81f8
Couto1411 <gabriel.couto14@hotmail.com>
2022-07-07 21:00:13 -0300

    teste function

--- js/script.js
+++ js/script.js
@@ -247,15 +247,17 @@ function resistTot() {
 
 function tensao() {
     var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-
-    output.innerHTML = slider.value;
+    var output = document.getElementById("tensao");
+    output.innerHTML = slider.value+" V";
+    if(document.getElementById("chave").style.borderRightColor!="black"){
+        document.getElementById("tensao-capac").innerHTML= slider.value;
+    }
 }
 
 function capacitor() {
     let valor = document.getElementById('input-capacitor')
 
-    capacitancia.innerHTML = valor.value
+    capacitancia.innerHTML = valor.value+ "μF";
 }
 
 function select(){
@@ -263,10 +265,62 @@ function select(){
         document.getElementById("chave").style.borderRight="0.4em solid black";
         document.getElementById("chave").style.borderBottom="0";
         document.getElementById("chave").style.borderBottomColor="transparent"
+        for (let i = 0; i <= 60; i++) {
+            console.log(document.getElementById("tensao-capac").innerHTML=(parseFloat(document.getElementById("tensao").innerHTML))*((Math.exp(1))**(-1*(i/(parseFloat(document.getElementById("resist-tot").innerHTML)*parseFloat(document.getElementById("capacitancia").innerHTML))))));    
+
+        }    
     }
     else{
         document.getElementById("chave").style.borderRight="0";
         document.getElementById("chave").style.borderRightColor="transparent"
         document.getElementById("chave").style.borderBottom="0.4em solid black";
+        document.getElementById("tensao-capac").innerHTML=parseFloat(document.getElementById("tensao").innerHTML);
     }
+}
+
+function fun1(x) {return (parseFloat(document.getElementById("tensao").innerHTML))*((Math.exp(1))**(-1*(x/(parseFloat(document.getElementById("resist-tot").innerHTML)*parseFloat(document.getElementById("capacitancia").innerHTML)))));  }
+
+function draw() {
+    var canvas = document.getElementById("canvas");
+    if (null==canvas || !canvas.getContext) return;
+
+    var axes={}, ctx=canvas.getContext("2d");
+    axes.x0 = .5 + .5*canvas.width;  // x0 pixels from left to x=0
+    axes.y0 = .5 + .5*canvas.height; // y0 pixels from top to y=0
+    axes.scale = 40;                 // 40 pixels from x=0 to x=1
+    axes.doNegativeX = true;
+
+    showAxes(ctx,axes);
+    funGraph(ctx,axes,fun1,"rgb(11,153,11)",1); 
+}
+
+function funGraph (ctx,axes,func,color,thick) {
+    var xx, yy, dx=4, x0=axes.x0, y0=axes.y0, scale=axes.scale;
+    var iMax = Math.round((ctx.canvas.width-x0)/dx);
+    var iMin = axes.doNegativeX ? Math.round(-x0/dx) : 0;
+    ctx.beginPath();
+    ctx.lineWidth = thick;
+    ctx.strokeStyle = color;
+
+    for (var i=iMin;i<=iMax;i++) {
+        xx = dx*i; yy = scale*func(xx/scale);
+        if (i==iMin) 
+            ctx.moveTo(x0+xx,y0-yy);
+        else         
+            ctx.lineTo(x0+xx,y0-yy);
+    }
+    ctx.stroke();
+}
+
+function showAxes(ctx,axes) {
+    var x0=axes.x0, w=ctx.canvas.width;
+    var y0=axes.y0, h=ctx.canvas.height;
+    var xmin = axes.doNegativeX ? 0 : x0;
+    ctx.beginPath();
+    ctx.strokeStyle = "rgb(128,128,128)"; 
+    ctx.moveTo(xmin,y0); 
+    ctx.lineTo(w,y0);  // X axis
+    ctx.moveTo(x0,0);    
+    ctx.lineTo(x0,h);  // Y axis
+    ctx.stroke();
 }
