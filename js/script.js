817411c4e1edf9f2d5f596a5e2e6c243629a7d17
Couto1411 <gabriel.couto14@hotmail.com>
2022-07-09 09:39:35 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var segundo=0.1;
+
 function hide() {
     document.getElementById("resistor-table1").style.display = "none";
     document.getElementById("resistor-table2").style.display = "none";
@@ -54,52 +56,78 @@ function cliqueCapacitor() {
     document.getElementById("regulador-tensao").style.display = "none";
     document.getElementById("form-capacitor").style.display = "inline";
 }
-function faixa1P() { document.getElementById("brand1").style.backgroundColor = "black"; document.getElementById("brand1").style.borderColor = "black"; resistTot(); }
-function faixa1M() { document.getElementById("brand1").style.backgroundColor = "brown"; document.getElementById("brand1").style.borderColor = "brown"; resistTot(); }
-function faixa1Vm() { document.getElementById("brand1").style.backgroundColor = "red"; document.getElementById("brand1").style.borderColor = "red"; resistTot(); }
-function faixa1L() { document.getElementById("brand1").style.backgroundColor = "orange"; document.getElementById("brand1").style.borderColor = "orange"; resistTot(); }
-function faixa1Am() { document.getElementById("brand1").style.backgroundColor = "yellow"; document.getElementById("brand1").style.borderColor = "yellow"; resistTot(); }
-function faixa1Vd() { document.getElementById("brand1").style.backgroundColor = "green"; document.getElementById("brand1").style.borderColor = "green"; resistTot(); }
-function faixa1Az() { document.getElementById("brand1").style.backgroundColor = "blue"; document.getElementById("brand1").style.borderColor = "blue"; resistTot(); }
-function faixa1Vi() { document.getElementById("brand1").style.backgroundColor = "violet"; document.getElementById("brand1").style.borderColor = "violet"; resistTot(); }
-function faixa1C() { document.getElementById("brand1").style.backgroundColor = "gray"; document.getElementById("brand1").style.borderColor = "gray"; resistTot(); }
-function faixa1B() { document.getElementById("brand1").style.backgroundColor = "white"; document.getElementById("brand1").style.borderColor = "white"; resistTot(); }
 
-function faixa2P() { document.getElementById("brand2").style.backgroundColor = "black"; document.getElementById("brand2").style.borderColor = "black"; resistTot(); }
-function faixa2M() { document.getElementById("brand2").style.backgroundColor = "brown"; document.getElementById("brand2").style.borderColor = "brown"; resistTot(); }
-function faixa2Vm() { document.getElementById("brand2").style.backgroundColor = "red"; document.getElementById("brand2").style.borderColor = "red"; resistTot(); }
-function faixa2L() { document.getElementById("brand2").style.backgroundColor = "orange"; document.getElementById("brand2").style.borderColor = "orange"; resistTot(); }
-function faixa2Am() { document.getElementById("brand2").style.backgroundColor = "yellow"; document.getElementById("brand2").style.borderColor = "yellow"; resistTot(); }
-function faixa2Vd() { document.getElementById("brand2").style.backgroundColor = "green"; document.getElementById("brand2").style.borderColor = "green"; resistTot(); }
-function faixa2Az() { document.getElementById("brand2").style.backgroundColor = "blue"; document.getElementById("brand2").style.borderColor = "blue"; resistTot(); }
-function faixa2Vi() { document.getElementById("brand2").style.backgroundColor = "violet"; document.getElementById("brand2").style.borderColor = "violet"; resistTot(); }
-function faixa2C() { document.getElementById("brand2").style.backgroundColor = "gray"; document.getElementById("brand2").style.borderColor = "gray"; resistTot(); }
-function faixa2B() { document.getElementById("brand2").style.backgroundColor = "white"; document.getElementById("brand2").style.borderColor = "white"; resistTot(); }
-
-function faixa3P() { document.getElementById("brand3").style.backgroundColor = "black"; document.getElementById("brand3").style.borderColor = "black"; resistTot(); }
-function faixa3M() { document.getElementById("brand3").style.backgroundColor = "brown"; document.getElementById("brand3").style.borderColor = "brown"; resistTot(); }
-function faixa3Vm() { document.getElementById("brand3").style.backgroundColor = "red"; document.getElementById("brand3").style.borderColor = "red"; resistTot(); }
-function faixa3L() { document.getElementById("brand3").style.backgroundColor = "orange"; document.getElementById("brand3").style.borderColor = "orange"; resistTot(); }
-function faixa3Am() { document.getElementById("brand3").style.backgroundColor = "yellow"; document.getElementById("brand3").style.borderColor = "yellow"; resistTot(); }
-function faixa3Vd() { document.getElementById("brand3").style.backgroundColor = "green"; document.getElementById("brand3").style.borderColor = "green"; resistTot(); }
-function faixa3Az() { document.getElementById("brand3").style.backgroundColor = "blue"; document.getElementById("brand3").style.borderColor = "blue"; resistTot(); }
-function faixa3Vi() { document.getElementById("brand3").style.backgroundColor = "violet"; document.getElementById("brand3").style.borderColor = "violet"; resistTot(); }
-function faixa3C() { document.getElementById("brand3").style.backgroundColor = "gray"; document.getElementById("brand3").style.borderColor = "gray"; resistTot(); }
-function faixa3B() { document.getElementById("brand3").style.backgroundColor = "white"; document.getElementById("brand3").style.borderColor = "white"; resistTot(); }
-function faixa3G() { document.getElementById("brand3").style.backgroundColor = "gold"; document.getElementById("brand3").style.borderColor = "gold"; resistTot(); }
-function faixa3S() { document.getElementById("brand3").style.backgroundColor = "silver"; document.getElementById("brand3").style.borderColor = "silver"; resistTot(); }
-
-function faixa5M() { document.getElementById("brand5").style.backgroundColor = "brown"; document.getElementById("brand5").style.borderColor = "brown"; resistTot(); }
-function faixa5Vm() { document.getElementById("brand5").style.backgroundColor = "red"; document.getElementById("brand5").style.borderColor = "red"; resistTot(); }
-function faixa5Vd() { document.getElementById("brand5").style.backgroundColor = "green"; document.getElementById("brand5").style.borderColor = "green"; resistTot(); }
-function faixa5Az() { document.getElementById("brand5").style.backgroundColor = "blue"; document.getElementById("brand5").style.borderColor = "blue"; resistTot(); }
-function faixa5Vi() { document.getElementById("brand5").style.backgroundColor = "violet"; document.getElementById("brand5").style.borderColor = "violet"; resistTot(); }
-function faixa5C() { document.getElementById("brand5").style.backgroundColor = "gray"; document.getElementById("brand5").style.borderColor = "gray"; resistTot(); }
-function faixa5G() { document.getElementById("brand5").style.backgroundColor = "gold"; document.getElementById("brand5").style.borderColor = "gold"; resistTot(); }
-function faixa5S() { document.getElementById("brand5").style.backgroundColor = "silver"; document.getElementById("brand5").style.borderColor = "silver"; resistTot(); }
+function faixaM(brand) {
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "brown"; document.getElementById(brand).style.borderColor = "brown"; resistTot(); }
+function faixaVm(brand) {
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "red"; document.getElementById(brand).style.borderColor = "red"; resistTot(); }
+function faixaVd(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "green"; document.getElementById(brand).style.borderColor = "green"; resistTot(); }
+function faixaL(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "orange"; document.getElementById(brand).style.borderColor = "orange"; resistTot(); }
+function faixaAm(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "yellow"; document.getElementById(brand).style.borderColor = "yellow"; resistTot(); }
+function faixaAz(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "blue"; document.getElementById(brand).style.borderColor = "blue"; resistTot(); }
+function faixaVi(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "violet"; document.getElementById(brand).style.borderColor = "violet"; resistTot(); }
+function faixaC(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "gray"; document.getElementById(brand).style.borderColor = "gray"; resistTot(); }
+function faixaB(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "white"; document.getElementById(brand).style.borderColor = "white"; resistTot(); }
+function faixaG(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "gold"; document.getElementById(brand).style.borderColor = "gold"; resistTot(); }
+function faixaS(brand) { 
+    if (brand==1) brand="brand1";
+    else if(brand==2) brand="brand2";  
+    else if(brand==3) brand="brand3"; 
+    else if(brand==5) brand="brand5";
+    document.getElementById(brand).style.backgroundColor = "silver"; document.getElementById(brand).style.borderColor = "silver"; resistTot(); }
 
 function resistTot() {
     let resistTot;
+    let append="";
+    let percentage="";
     let brand1 = document.getElementById("brand1").style.backgroundColor;
     if (brand1) {
         switch (brand1) {
@@ -180,25 +208,29 @@ function resistTot() {
                 resistTot += "00";
                 break;
             case "orange":
-                resistTot += "K";
+                append = "K";
                 break;
             case "yellow":
-                resistTot += "0K";
+                resistTot += "0";
+                append="K";
                 break;
             case "green":
-                resistTot += "00K";
+                resistTot += "00";
+                append="K";
                 break;
             case "blue":
-                resistTot += "M";
+                append="M";
                 break;
             case "violet":
-                resistTot += "0M";
+                resistTot += "0";
+                append="M";
                 break;
             case "gray":
-                resistTot += "00M";
+                resistTot += "00";
+                append="M";
                 break;
             case "white":
-                resistTot += "G";
+                append="G";
                 break;
             case "gold":
                 resistTot = (parseInt(resistTot) / 10)
@@ -209,39 +241,47 @@ function resistTot() {
             default:
                 break;
         }
-        resistTot += " Ω";
         let brand5 = document.getElementById("brand5").style.backgroundColor;
         if (brand5) {
             switch (brand5) {
                 case "brown":
-                    resistTot += " ± 1%";
+                    percentage = " ± 1%";
                     break;
                 case "red":
-                    resistTot += " ± 2%";
+                    percentage = " ± 2%";
                     break;
                 case "green":
-                    resistTot += " ± 0.5%";
+                    percentage = " ± 0.5%";
                     break;
                 case "blue":
-                    resistTot += " ± 0.25%";
+                    percentage = " ± 0.25%";
                     break;
                 case "violet":
-                    resistTot += " ± 0.1%";
+                    percentage = " ± 0.1%";
                     break;
                 case "gray":
-                    resistTot += " ± 0.05%";
+                    percentage = " ± 0.05%";
                     break;
                 case "gold":
-                    resistTot += " ± 5%";
+                    percentage = " ± 5%";
                     break;
                 case "silver":
-                    resistTot += " ± 0.01%";
+                    percentage = " ± 0.01%";
                     break;
                 default:
                     break;
             }
         }
         document.getElementById("resist-tot").innerText = resistTot;
+        if (percentage.length>1) {
+            percentage+=" Ω";
+            document.getElementById("percentage").innerText = percentage;
+            document.getElementById("append").innerText = append;
+        }
+        else{
+            append+=" Ω";
+            document.getElementById("append").innerText = append;
+        }
     }
 }
 
@@ -264,63 +304,38 @@ function select(){
     if(document.getElementById("chave").style.borderRightColor=="transparent"){
         document.getElementById("chave").style.borderRight="0.4em solid black";
         document.getElementById("chave").style.borderBottom="0";
-        document.getElementById("chave").style.borderBottomColor="transparent"
-        for (let i = 0; i <= 60; i++) {
-            console.log(document.getElementById("tensao-capac").innerHTML=(parseFloat(document.getElementById("tensao").innerHTML))*((Math.exp(1))**(-1*(i/(parseFloat(document.getElementById("resist-tot").innerHTML)*parseFloat(document.getElementById("capacitancia").innerHTML))))));    
-
-        }    
+        document.getElementById("chave").style.borderBottomColor="transparent";
+        setTimeout(tensaCapac,1000); 
+        segundo=0;
     }
     else{
         document.getElementById("chave").style.borderRight="0";
-        document.getElementById("chave").style.borderRightColor="transparent"
+        document.getElementById("chave").style.borderRightColor="transparent";
         document.getElementById("chave").style.borderBottom="0.4em solid black";
         document.getElementById("tensao-capac").innerHTML=parseFloat(document.getElementById("tensao").innerHTML);
     }
 }
 
-function fun1(x) {return (parseFloat(document.getElementById("tensao").innerHTML))*((Math.exp(1))**(-1*(x/(parseFloat(document.getElementById("resist-tot").innerHTML)*parseFloat(document.getElementById("capacitancia").innerHTML)))));  }
-
-function draw() {
-    var canvas = document.getElementById("canvas");
-    if (null==canvas || !canvas.getContext) return;
-
-    var axes={}, ctx=canvas.getContext("2d");
-    axes.x0 = .5 + .5*canvas.width;  // x0 pixels from left to x=0
-    axes.y0 = .5 + .5*canvas.height; // y0 pixels from top to y=0
-    axes.scale = 40;                 // 40 pixels from x=0 to x=1
-    axes.doNegativeX = true;
-
-    showAxes(ctx,axes);
-    funGraph(ctx,axes,fun1,"rgb(11,153,11)",1); 
-}
-
-function funGraph (ctx,axes,func,color,thick) {
-    var xx, yy, dx=4, x0=axes.x0, y0=axes.y0, scale=axes.scale;
-    var iMax = Math.round((ctx.canvas.width-x0)/dx);
-    var iMin = axes.doNegativeX ? Math.round(-x0/dx) : 0;
-    ctx.beginPath();
-    ctx.lineWidth = thick;
-    ctx.strokeStyle = color;
-
-    for (var i=iMin;i<=iMax;i++) {
-        xx = dx*i; yy = scale*func(xx/scale);
-        if (i==iMin) 
-            ctx.moveTo(x0+xx,y0-yy);
-        else         
-            ctx.lineTo(x0+xx,y0-yy);
+function tensaCapac(){
+    if(document.getElementById("chave").style.borderRightColor=="black"){
+        let multiplier=1;
+        let append = document.getElementById("append").innerHTML;
+        switch (append) {
+            case "K":
+                multiplier=1000
+                break;
+            case "M":
+                multiplier=1000000
+                break;
+            case "G":
+                multiplier=1000000000
+                break;
+            default:
+                break;
+        }
+        document.getElementById("tensao-capac").innerHTML=((parseFloat(document.getElementById("tensao").innerHTML))*((Math.exp(1))**(-1*(segundo/((parseFloat(document.getElementById("resist-tot").innerHTML)*(multiplier))*(parseFloat(document.getElementById("capacitancia").innerHTML)*(10**(-6))))))).toFixed(5));
+        segundo+=0.1;
+        if(parseFloat(document.getElementById("tensao-capac").innerHTML)>0.00001)
+            setTimeout(tensaCapac, 1000);
     }
-    ctx.stroke();
-}
-
-function showAxes(ctx,axes) {
-    var x0=axes.x0, w=ctx.canvas.width;
-    var y0=axes.y0, h=ctx.canvas.height;
-    var xmin = axes.doNegativeX ? 0 : x0;
-    ctx.beginPath();
-    ctx.strokeStyle = "rgb(128,128,128)"; 
-    ctx.moveTo(xmin,y0); 
-    ctx.lineTo(w,y0);  // X axis
-    ctx.moveTo(x0,0);    
-    ctx.lineTo(x0,h);  // Y axis
-    ctx.stroke();
 }
